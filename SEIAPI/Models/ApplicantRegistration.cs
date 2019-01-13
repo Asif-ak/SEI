32759ca33f55a7419b2cd8840968ceb69d21855d
asif <iu.asifak@gmail.com>
2019-01-12 18:45:18 -0800

    first model

---
+++ SEIAPI/Models/ApplicantRegistration.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace SEIAPI.Models
+{
+    [Table("A")]
+    public class ApplicantRegistration
+    {
+        [Key,DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID { get; set; }
+        [Required,MaxLength(15,ErrorMessage ="First Name length cannot exceed 15 characters")]
+        public string fname { get; set; }
+        [Required, MaxLength(15, ErrorMessage = "Last Name length cannot exceed 15 characters")]
+        public string lname { get; set; }
+
+    }
+}
